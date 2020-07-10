5e2f4e81223bc8200d2d512d1b89aa44e95f36d1
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-07-10 10:43:03 +0200

    aligned on jx v2.1.97

--- pkg/jenkinsutil/helpers.go
+++ pkg/jenkinsutil/helpers.go
@@ -11,7 +11,7 @@ import (
 	"github.com/jenkins-x/jx/v2/pkg/builds"
 	"github.com/jenkins-x/jx/v2/pkg/gits"
 	jxjenkins "github.com/jenkins-x/jx/v2/pkg/jenkins"
-	"github.com/jenkins-x/jx/v2/pkg/log"
+	"github.com/jenkins-x/jx-logging/pkg/log"
 	"github.com/jenkins-x/jx/v2/pkg/util"
 	"github.com/spf13/cobra"
 )
