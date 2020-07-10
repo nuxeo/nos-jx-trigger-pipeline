a554a6f9566dab9c78710355b02a6823a465efd7
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-07-10 10:43:03 +0200

    aligned on jx v2.1.97

--- pkg/cmd/server/server_add.go
+++ pkg/cmd/server/server_add.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jenkins-x/jx/v2/pkg/cmd/helper"
 	"github.com/jenkins-x/jx/v2/pkg/cmd/templates"
 	"github.com/jenkins-x/jx/v2/pkg/kube/naming"
-	"github.com/jenkins-x/jx/v2/pkg/log"
+	"github.com/jenkins-x/jx-logging/pkg/log"
 	"github.com/jenkins-x/jx/v2/pkg/util"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
