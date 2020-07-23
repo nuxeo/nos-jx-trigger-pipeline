279559377abcee84ab8dcd02afc21cb10ba10138
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-07-23 11:19:33 +0200

    fixup promote

--- promote.sh
+++ promote.sh
@@ -2,6 +2,6 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-jx step create pr go --name github.com/nuxeolabs/trigger-pipeline --version ${VERSION} --build "make build" --repo https://github.com/nuxeo/jxlabs-nos-jxl.git
+jx step create pr go --name github.com/nuxeo/jxlabs-nos-trigger-pipeline --version ${VERSION} --build "make build" --repo https://github.com/nuxeo/jxlabs-nos-jxl.git
 
 jx step create pr regex --regex "^(?m)\s+name: tp\s+version: \"(.*)\"$"  --version ${VERSION} --files alpha/plugins.yml --repo https://github.com/nuxeo/jxlabs-nos-jxl.git
